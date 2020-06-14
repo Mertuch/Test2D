3b6aa52fb8a7f9bbd76f5032b4ff0ad5e2912ff4
Mertuch <mertucha@o2.pl>
2020-06-14 15:10:36 +0200

    Lab7

---
+++ Assets/test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public int x = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+       for(int i=0;i<x;i++) print("Adam");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
